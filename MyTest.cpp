86ae8ea5413a7f3cec663f4020cf7849744c3122
YTL2814454117 <2814454117@qq.com>
2022-08-08 11:07:29 +0800

    grayScale

--- MyTest.cpp
+++ MyTest.cpp
@@ -7,9 +7,9 @@ using namespace std; // 规定命名空间
 // 程序入口
 int main(int argc, char **argv)
 {
-    Mat src = imread("/home/ytl/opencv-4.6.0/1.jpg"); // 读入图片信息并保存在矩阵中
-    imshow("input", src);                             // 在input窗口显示图片
-    waitKey(0);                                       // 等待图片展示
-    destroyAllWindows;                                // 清空所有窗口
-    return 0;                                         // 程序结束
+    Mat src = imread("/home/ytl/opencv-4.6.0/1.jpg", IMREAD_GRAYSCALE); // 读入图片信息并保存在矩阵中
+    imshow("input", src);                                               // 在input窗口显示图片
+    waitKey(0);                                                         // 等待图片展示
+    destroyAllWindows;                                                  // 清空所有窗口
+    return 0;                                                           // 程序结束
 }
